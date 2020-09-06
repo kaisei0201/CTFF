ce7f179fa88b7b21dbfaa6cabe56d66ce0d60788
kaisei0201 <kaisei0201ok@gmail.com>
2020-09-06 19:24:33 +0900

    Font AwesomeのURLを変更

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,7 +14,7 @@
         <title>@yield('title')</title>
         {{-- Laravel標準JavaScriptを読み込む --}}
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="https://kit.fontawesome.com/ffcd300ab8.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/423dea4db0.js" crossorigin="anonymous"></script>
         {{-- Fonts --}}
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
